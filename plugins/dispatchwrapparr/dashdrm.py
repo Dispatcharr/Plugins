6d5a96150c39656871907c3b51b72b372dd5a76a
Jordan Dalley <jordan@dalleyfamily.net>
2026-08-29 18:14:13 +1200

    [dispatchwrapparr]: Bump version to 1.7.7 (#261)

--- plugins/dispatchwrapparr/dashdrm.py
+++ plugins/dispatchwrapparr/dashdrm.py
@@ -19,7 +19,7 @@ from requests.adapters import HTTPAdapter
 
 log = logging.getLogger(__name__)
 
-__version__ = "1.7.6"
+__version__ = "1.7.7"
 
 '''
 DASHDRM plugin for Dispatchwrapparr & Streamlink
